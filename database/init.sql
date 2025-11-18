f9bc8802645a35b30a5b6b6e86c7009b403c6c79
adriansanchez4 <adrian.sanchezelvira@riberadeltajo.es>
2025-11-18 11:48:58 +0100

    fix: base de datos con CHARSET y ENGINE e INDEX

--- database/init.sql
+++ database/init.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     password_hash VARCHAR(200), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Crear tabla de productos
 CREATE TABLE IF NOT EXISTS productos (
@@ -24,31 +24,34 @@ CREATE TABLE IF NOT EXISTS productos (
     precio DECIMAL(7, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS pedidos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     id_user BIGINT NOT NULL,
-    FOREIGN KEY (id_user) REFERENCES usuarios(id),
+    INDEX idx_pedidos_id_user (id_user),
+    CONSTRAINT fk_pe_usuarios FOREIGN KEY (id_user) REFERENCES usuarios(id),
     precio_total DECIMAL(9,2) NOT NULL,
     metodo_pago ENUM ('tarjeta','bizum','transferencia','paypal') DEFAULT 'tarjeta',
     estado ENUM ('entregado','pendiente','enviado','cancelado', 'carrito') DEFAULT 'carrito',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS pedidos_productos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     cantidad BIGINT NOT NULL,
     precio_unitario DECIMAL(7,2) NOT NULL,
-    id_pedido BIGINT NOT NULL REFERENCES pedidos(id),
-    FOREIGN KEY (id_pedido) REFERENCES pedidos(id),
+    id_pedido BIGINT NOT NULL,
+    INDEX idx_pp_id_pedido (id_pedido),
+    CONSTRAINT fk_pp_pedidos FOREIGN KEY (id_pedido) REFERENCES pedidos(id),
     id_producto BIGINT NOT NULL,
-    FOREIGN KEY (id_producto) REFERENCES productos(id)
-);
+    INDEX idx_pp_id_producto (id_producto),
+    CONSTRAINT fk_pp_productos FOREIGN KEY (id_producto) REFERENCES productos(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Insertar datos de ejemplo
-INSERT INTO users (nombre_completo, email) VALUES
+INSERT INTO usuarios (nombre_completo, email) VALUES
     ('Juan Perez', 'juan.perez@example.com'),
     ('Maria Garcia', 'maria.garcia@example.com'),
     ('Carlos Lopez', 'carlos.lopez@example.com')
